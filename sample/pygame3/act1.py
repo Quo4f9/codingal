67920c0729fbb572003fd79d54c1e7b6d22042f1
Quo4f9 <quo7890@gmail.com>
2026-07-06 18:49:27 +0300

    yes

--- sample/pygame3/act1.py
+++ sample/pygame3/act1.py
@@ -28,10 +28,10 @@ class Sprite(pygame.sprite.Sprite):
         self.rect.move_ip(self.velocity)
         boundary_hit=False
         if self.rect.left <=0 or self.rect.right >=500:
-            self.velocity[0] = self.velocity[0]
+            self.velocity[0] = -self.velocity[0]
             boudary_hit=True
         if self.rect.top <=0 or self.rect.bottom >=400:
-            self.velocity[1] = self.velocity[1]
+            self.velocity[1] = -self.velocity[1]
             boudary_hit=True
 
         if boundary_hit:
@@ -58,18 +58,18 @@ exit=False
 clock=pygame.time.Clock()
 
 while not exit :
-     for event in pygame.event.get():
+    for event in pygame.event.get():
           if event.type==pygame.QUIT:
                exit=True
           elif event.type==SPRITE_COLOR_CHANGE_EVENT:
                spl.change_color()
           elif event.type==BACKGROUND_COLOR_CHANGE_EVENT:
-               change_background_color()
-all_sprites_list.update()
-screen.fill(bg_color)
-all_sprites_list.draw(screen)
-pygame.display.flip()
-clock.tick(240)
+                change_background_color()
+    all_sprites_list.update()
+    screen.fill(bg_color)
+    all_sprites_list.draw(screen)
+    pygame.display.flip()
+    clock.tick(240)
 pygame.quit()
         
                
